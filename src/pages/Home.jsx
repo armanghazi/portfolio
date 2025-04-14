607110c459053dd44c2bf80efd0625353463117a
Arman Ghaziaskari <103433612+ghaziaskari@users.noreply.github.com>
2025-04-14 21:27:59 +0200

    updateHome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,11 @@ const Home = () => {
         <div className="user-text">
         <h1>About Me</h1>
           <p>
-          Hello! I'm a passionate technologist with a compelling blend of skills in Data Science,Geographic Information Systems (GIS), and Web Development. I thrive on the process of analyzing data to discover valuable patterns and insights, which I then translate into practical and innovative solutions.
-          </p>
+          Hello! I'm a passionate technologist with a unique blend of skills in Data Science, Geographic Information Systems (GIS), and Web Development. I thrive on analyzing data to uncover meaningful patterns and insights, which I then transform into practical and innovative solutions.          </p>
           <p>
-          My initial foray into data analysis began with GIS and Remote Sensing, where I learned to interpret and visualize spatial data. This foundational understanding has been significantly enhanced by my deep dive into Data Science, providing me with the tools and techniques to perform sophisticated analysis and build predictive models. My ability to combine these analytical skills with Web Development allows me to bring data-driven applications to life on the web.
-          </p>
+          My journey into data analysis began with GIS and Remote Sensing, where I developed a strong foundation in interpreting and visualizing spatial data. This experience was further enriched through my deep dive into Data Science, equipping me with advanced tools and techniques for sophisticated analysis and predictive modeling. By combining these analytical skills with Web Development—particularly using JavaScript—I bring data-driven applications to life on the web.          </p>
           <p>
-          What truly excites me is building interactive web applications that harness the power of data science to explore and present geographic information. I'm driven to create engaging tools that enable organizations to gain deeper understanding from their data,make evidence-based decisions, and solve real-world challenges effectively.
-            </p>
+          What truly excites me is building interactive web applications that harness the power of data science to explore and present geographic information. I'm driven to create engaging tools that help organizations gain deeper insights, make informed decisions, and solve real-world challenges effectively.            </p>
           <div className="cv-buttons">
             <a href="./pdfs/english.pdf" className="cv-button" target="_blank" rel="noopener noreferrer">
               English Curriculum
