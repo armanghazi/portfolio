60e7ffb7e1c1de12178737be7712c6a1e4ce3e86
Arman Ghaziaskari <103433612+ghaziaskari@users.noreply.github.com>
2026-07-28 14:36:15 +0200

    chore(deps): upgrade React and React Router dependencies to versions 19.2.7 and 8.3.0 respectively, and remove react-router-dom for improved routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 import LanguageSwitcher from './LanguageSwitcher';
